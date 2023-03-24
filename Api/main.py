6adb24758185d139922aa488386345f11eb92dbd
Κωνσταντίνος Καρακασίδης <k.karakasidis@dimos.thermis.local>
2023-03-24 13:20:27 +0200

    Παρ 24/03/2023 script save

--- Api/main.py
+++ Api/main.py
@@ -3,7 +3,7 @@ from Api.apps import allScrap
 
 @app.get('/')
 def home():
-    text = 'sport links search machine API By Defender'
+    text = 'sport links, search machine API v1.0.0 By Defender'
     return text
 
 @app.get('/links')
